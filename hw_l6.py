f906b8ee73451c6dc334e8ecc8abb125efc4ec7b
aleksandr kuznetsov <anarond@gmail.com>
2025-11-25 10:45:30 +0300

    def add_short_body def clean_body_text

--- hw_l6.py
+++ hw_l6.py
@@ -10,8 +10,26 @@ def normalize_addresses(addresses):
     """
     Возвращает значение, в котором адрес приведен к нижнему регистру и очищен от пробелов по краям.
     """
-    return addresses.strip().lower()
+    text = addresses.strip().lower()
+    return text
+print(("1 ") + normalize_addresses(email5["from"]))
+print(("1 ") + normalize_addresses(email5["to"]))
 
-print(normalize_addresses(email5["from"]))
-print(normalize_addresses(email5["to"]))
+def add_short_body(email):
+    """
+    Возвращает email с новым ключом email["short_body"] —
+    первые 10 символов тела письма + "...".
+    """
+    email_body = email["body"]
+    email_body = email_body[:10] + "..."
+    return email_body
+print(("2 ") + add_short_body(email5))
 
+def clean_body_text(body):
+    """
+    Заменяет табы и переводы строк на пробелы.
+    """
+    clean_body = email5["body"]
+    clean_body = clean_body.replace("\n", " ").replace("\t", " ")
+    return clean_body
+print(("3 ") + clean_body_text(email5))
