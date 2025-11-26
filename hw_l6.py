b3c480233b9b8cf94340c1cd324c5caf7fe2e4b7
aleksandr kuznetsov <anarond@gmail.com>
2025-11-26 10:12:25 +0300

    def build_sent_text def check_empty_fields

--- hw_l6.py
+++ hw_l6.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 # 1. Создайте словарь email
 email5 = {
     "subject": "Project collaboration",
@@ -33,3 +35,33 @@ def clean_body_text(body):
     clean_body = clean_body.replace("\n", " ").replace("\t", " ")
     return clean_body
 print(("3 ") + clean_body_text(email5))
+
+def build_sent_text(email: dict) -> str:
+    """
+    Формирует текст письма в формате:
+
+    Кому: {to}, от {from}
+    Тема: {subject}, дата {date}
+    {clean_body}
+    """
+    a = f"Кому: {normalize_addresses(email5['to'])}, от {normalize_addresses(email5['from'])}"
+    b = f"Тема: {(email['subject'])}, дата {date.today()}"
+    c = clean_body_text(email5)
+    return f"{a}\n{b}\n{c}"
+print(("4 ") + build_sent_text(email5))
+
+def check_empty_fields(subject: str, body:str) -> tuple[bool, bool]:
+    """
+    Возвращает кортеж (is_subject_empty, is_body_empty).
+    True, если поле пустое.
+    """
+    is_subject_empty = not subject.strip()
+    is_body_empty = not body.strip()
+    return is_subject_empty, is_body_empty
+print(("5 ") + str(check_empty_fields(email5["subject"], email5["body"])))
+
+def mask_sender_email(login: str, domain: str) -> str:
+    """
+    Возвращает маску email: первые 2 символа логина + "***@" + домен.
+    """
+    return f"{login}[:2] + '***@' + {domain}"
